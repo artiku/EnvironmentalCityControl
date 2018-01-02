e903e6e2c229b2bf958f48b50630fe442070ca58
Artur Bassov <artbas@ttu.ee>
2018-01-02 23:51:31 +0200

    Maintenance now is logging info into a file.

--- src/city/crossroad/MaintenanceCrossroad.java
+++ src/city/crossroad/MaintenanceCrossroad.java
@@ -2,24 +2,47 @@ package city.crossroad;
 
 import car.Car;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MaintenanceCrossroad extends Crossroad {
 
     private AtomicInteger carNumberInQueue = new AtomicInteger(0);
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("mm:ss.SSS");
 
     public MaintenanceCrossroad() {
     }
 
     @Override
     public void driveThrough(Car car) {
-        System.out.println(String.format("Car #%s on the maintenance", car.getCarId()));
         try {
+            System.out.println(String.format("Car #%s on the maintenance", car.getCarId()));
+
             int timeToWait = 50 * carNumberInQueue.incrementAndGet();
             Thread.sleep(timeToWait);
             carNumberInQueue.decrementAndGet();
-            System.out.println(String.format("%s finished maintenance at #%s crossroad and waited for %s ms", car, this.getCrossroadId(), timeToWait));
+
+            String textInfo = String.format("%s finished maintenance at #%s crossroad and waited for %s ms\n",
+                    car, this.getCrossroadId(), timeToWait);
+            System.out.print(textInfo);
+
+            writeIntoLogFile(textInfo, "logfiles/finished_maintenance.txt");
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeIntoLogFile(String textInfo, String filename) {
+        try {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            textInfo = sdf.format(timestamp) + ": " + textInfo;
+            Files.write(Paths.get(filename), textInfo.getBytes(), StandardOpenOption.APPEND);
+        }catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/maintenance_centre/MaintenanceCentre.java
+++
@@ -1,4 +0,0 @@
-package maintenance_centre;
-
-public class MaintenanceCentre {
-}
